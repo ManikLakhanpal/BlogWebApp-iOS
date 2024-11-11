5e7f86dc206947ab9c5bab6527f5669a8d5d0eea
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-11-11 23:28:06 +0530

    Initial Commit

---
+++ BlogWebApp-iOS/BlogWebApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogWebApp_iOSApp.swift
+//  BlogWebApp-iOS
+//
+//  Created by Manik Lakhanpal on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BlogWebApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlogWebApp-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlogWebApp-iOS
+//
+//  Created by Manik Lakhanpal on 11/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
