4c8c24974b04a6b1b84a15ff754fd1cee0cce19e
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2025-01-21 20:22:45 +0530

    Home View added

--- BlogWebApp-iOS/ContentView.swift
+++ BlogWebApp-iOS/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
-            Text("House Selected")
+            HomeView()
                 .tabItem {
                     Label("Home", systemImage: "house")
                 }
