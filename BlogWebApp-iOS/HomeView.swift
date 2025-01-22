c776b25cdd9b02554a97363e66aeb2bf0f755b6b
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2025-01-22 09:46:18 +0530

    PostView added

--- BlogWebApp-iOS/HomeView.swift
+++ BlogWebApp-iOS/HomeView.swift
@@ -11,7 +11,29 @@ struct HomeView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                Text("Successfully selected HomeView")
+                VStack {
+                    HStack(alignment: .center) {
+                        
+                        Image(.manik)
+                            .resizable()
+                            .frame(width: 60, height: 60)
+                            .clipShape(Circle())
+                        
+                        VStack(alignment: .leading, spacing: 0.0) {
+                            Text("Manik Lakhanpal")
+                                .font(.title2)
+                                .fontWeight(.medium)
+                            Text("@w16manik")
+                                .font(.subheadline)
+                        }
+                        .padding(.leading)
+                        
+                        Spacer()
+                        
+                    }
+                    .padding(.leading)
+                }
+                .frame(width: .infinity)
             }
             .toolbar {
                 CustomToolbar(
