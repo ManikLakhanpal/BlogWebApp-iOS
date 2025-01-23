a363e98c6ca1c319d518d086be3a59c1b1361404
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2025-01-23 10:09:15 +0530

    Comments added to the content view file

--- BlogWebApp-iOS/ContentView.swift
+++ BlogWebApp-iOS/ContentView.swift
@@ -9,22 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        // Different TabViews for different pages
         TabView {
+            // Main View with all the posts
             HomeView()
                 .tabItem {
                     Label("Home", systemImage: "house")
                 }
             
+            // Currently work in progress
             Text("Notifications Selected")
                 .tabItem {
                     Label("Notification", systemImage: "bell")
                 }
             
+            // Currently work in progress
             Text("Message Selected")
                 .tabItem {
                     Label("Message", systemImage: "message")
                 }
-
+            
+            // Profile of the current User is here
             ProfileView()
                 .tabItem {
                     Label("Profile", systemImage: "person")
